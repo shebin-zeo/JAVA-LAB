396e1d3f040361470f41189e831698e9f1776e17
Shebin-Zeo <Shebin2024@gmail.com>
2024-02-29 11:05:20 +0530

    New file created

---
+++ Add.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class Add {
+    public static void main(String arg[])
+    {
+    Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the numbers");
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int result=num1+num2;
+        System.out.println("The Sum of two number is "+result);
+   
+    
+    
+}}
